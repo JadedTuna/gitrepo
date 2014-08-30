58bee054f57d0a0e83dd6826435ec5fce255eb42
Victor <digitloft@gmail.com>
2014-08-30 12:45:14 +0200

    Update gitrepo.py

--- gitrepo.py
+++ gitrepo.py
@@ -12,6 +12,23 @@ try:
 except ImportError:
     from StringIO  import StringIO
 
+class Delegate (object):
+    def __init__(self):
+        self.selrow = None
+    
+    def tableview_did_select(self, tableview, section, row):
+        # Called when a row was selected.
+        self.selrow = row
+        tableview.superview.close()
+
+    def tableview_did_deselect(self, tableview, section, row):
+        # Called when a row was de-selected (in multiple selection mode).
+        pass
+
+    def tableview_title_for_delete_button(self, tableview, section, row):
+        # Return the title for the 'swipe-to-***' button.
+        return 'Delete'
+
 repolink = "https://github.com/%(user)s/%(repo)s/archive/master.zip"
 releaselink = "https://api.github.com/repos/%(user)s/%(repo)s/releases"
 
@@ -66,7 +83,15 @@ def download_release(username, repo, unzip):
         return
     
     else:
-        lastrls = data[0]
+        vers = [i["tag_name"] for i in data]
+        rview = release_view(vers)
+        rview.present("sheet")
+        rview.wait_modal()
+        
+        row = rview["rtable"].delegate.selrow
+        if row is None: return
+        
+        lastrls = data[row]
         zipurl  = lastrls["zipball_url"]
         page = urllib2.urlopen(zipurl)
         data = page.read()
@@ -95,10 +120,24 @@ def gitdownload(button):
         download_repo(username, reponame, unzip)
     console.hide_activity()
 
+def release_view(releases):
+    rview = ui.View(name="Choose release")
+    lst   = ui.ListDataSource(releases)
+    delegate = Delegate()
+    
+    table = ui.TableView()
+    table.name = "rtable"
+    table.flex = "WH"
+    table.data_source = lst
+    table.delegate = delegate
+    
+    rview.add_subview(table)
+    return rview
+
 view = ui.load_view('gitrepo')
 cb = clipboard.get().strip()
 parse = urlparse.urlparse(cb)
-if parse.netloc in ("www.github.com", "github.com"):
+if parse.scheme:
     path = [i for i in parse.path.split("/") if i]
     if len(path) >= 2:
         view.subviews[2].text = path[0] # Username
