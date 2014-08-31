2d4811a7104fb08ae136e5dfda1e1cc79e14efc5
cclauss <cclauss@bluewin.ch>
2014-09-01 00:07:26 +0400

    Tighten up the code

--- gitrepo.py
+++ gitrepo.py
@@ -1,12 +1,6 @@
 # coding: utf-8
 
-import ui
-import json
-import urllib2
-import console
-import zipfile
-import urlparse
-import clipboard
+import clipboard, console, requests, ui, urlparse, zipfile
 try:
     from cStringIO import StringIO
 except ImportError:
@@ -14,24 +8,18 @@ except ImportError:
 
 class Delegate (object):
     def __init__(self):
-        self.selrow = None
+        self.selected_item = None
     
     def tableview_did_select(self, tableview, section, row):
-        # Called when a row was selected.
-        self.selrow = row
+        self.selected_item = tableview.data_source.items[row]
         tableview.superview.close()
 
-    def tableview_did_deselect(self, tableview, section, row):
-        # Called when a row was de-selected (in multiple selection mode).
-        pass
+repolink    = "https://github.com/{}/{}/archive/master.zip"
+browselink  = "https://api.github.com/users/{}/repos"
+releaselink = "https://api.github.com/repos/{}/{}/releases"
 
-    def tableview_title_for_delete_button(self, tableview, section, row):
-        # Return the title for the 'swipe-to-***' button.
-        return 'Delete'
-
-repolink    = "https://github.com/%(user)s/%(repo)s/archive/master.zip"
-browselink  = "https://api.github.com/users/%(user)s/repos"
-releaselink = "https://api.github.com/repos/%(user)s/%(repo)s/releases"
+def error_alert(msg="General error"):
+    console.alert("Error", msg, "OK", hide_cancel_button=True)
 
 @ui.in_background
 def save_zip(data, name, unzip):
@@ -45,76 +33,43 @@ def save_zip(data, name, unzip):
 
 @ui.in_background
 def download_repo(username, repo, unzip):
-    formatdict = {
-        "user": username,
-        "repo": repo
-    }
-    
+    url = repolink.format(username, repo)
     try:
-        page = urllib2.urlopen(repolink % formatdict)
-    except urllib2.HTTPError as err:
-        console.alert("Error", "Repo not found", "Ok", hide_cancel_button=True)
-        return
-    except:
-        console.alert("Error", "Error downloading repo", "Ok", hide_cancel_button=True)
-        return
-        
-    data = page.read()
-    page.close()
-    save_zip(data, repo, unzip)
+        save_zip(requests.get(url).content, repo, unzip)
+    except Exception as err:
+        return error_alert("Error downloading repo: {}".format(err))
     console.hud_alert("Done!")
 
 @ui.in_background
 def download_release(username, repo, unzip):
-    formatdict = {
-        "user": username,
-        "repo": repo
-    }
-    
-    page = urllib2.urlopen(releaselink % formatdict)
-    data = json.load(StringIO(page.read()))
-    page.close()
-    
+    url = releaselink.format(username, repo)
+    data = requests.get(url).json()
     if not data:
-        console.alert("Error", "This repo has no releases", "Ok", hide_cancel_button=True)
-        return
-    
+        return error_alert("This repo has no releases: " + url)
     elif "message" in data and data["message"] == "Not Found":
-        console.alert("Error", "Repo not found", "Ok", hide_cancel_button=True)
-        return
-    
-    else:
-        vers = sorted([i["tag_name"] for i in data])
-        rview = release_view(vers)
-        rview.present("sheet")
-        rview.wait_modal()
-        
-        row = rview["rtable"].delegate.selrow
-        if row is None: return
-        
-        lastrls = data[row]
-        zipurl  = lastrls["zipball_url"]
-        page = urllib2.urlopen(zipurl)
-        data = page.read()
-        page.close()
-        save_zip(data, lastrls["name"], unzip)
-        console.hud_alert("Done!")
+        return error_alert("Repo '{}' not found".format(repo))
+    vers = sorted([i["tag_name"] for i in data])
+    rview = data_view("release", vers)
+    rview.present("sheet")
+    rview.wait_modal()
+    tapped_text = rview["rtable"].delegate.selected_item
+    if tapped_text:
+        for d in data:
+            if d["tag_name"] == tapped_text:
+                zipurl = d["zipball_url"]
+                save_zip(requests.get(zipurl).content, tapped_text, unzip)
+                return console.hud_alert("Done!")
 
 @ui.in_background
 def gitdownload(button):
     isrelease = view["sgcontrol"].selected_index
-    username  = view["username"].text
-    reponame  = view["reponame"].text
+    username  = view["username"].text = view["username"].text.strip()
+    reponame  = view["reponame"].text = view["reponame"].text.strip()
     unzip     = view["dounzip"].value
-    
-    if not username.strip():
-        console.alert("Error", "Please enter username", "Ok", hide_cancel_button=True)
-        return
-    
-    if not reponame.strip():
-        console.alert("Error", "Please enter repo name", "Ok", hide_cancel_button=True)
-        return
-    
+    if not username:
+        return error_alert("Please enter username")
+    if not reponame:
+        return error_alert("Please enter repo name")
     console.show_activity()
     if isrelease:
         download_release(username, reponame, unzip)
@@ -124,75 +79,43 @@ def gitdownload(button):
 
 @ui.in_background
 def gitbrowse(sender):
-    username  = view["username"].text
-    if not username.strip():
-        console.alert("Error", "Please enter username", "Ok", hide_cancel_button=True)
-        return
-    
-    formatdict = {
-        "user": username
-    }
+    username = view["username"].text = view["username"].text.strip()
+    if not username:
+        return error_alert("Please enter username")
+    url = browselink.format(username)
     try:
-        page = urllib2.urlopen(browselink % formatdict)
-    except urllib2.HTTPError as err:
-        console.alert("Error", "User not found", "Ok", hide_cancel_button=True)
-        return
-    except:
-        console.alert("Error", "Error downloading metadata", "Ok", hide_cancel_button=True)
-        return
-    data = json.load(StringIO(page.read()))
-    page.close()
-    
-    if "message" in data and data["message"] == "Not Found":
-        console.alert("Error", "User not found", "Ok", hide_cancel_button=True)
-        return
-    
-    else:
-        repos = sorted([i["name"] for i in data])
-        rview = repo_view(repos)
-        rview.present("sheet")
-        rview.wait_modal()
-        
-        row = rview["rtable"].delegate.selrow
-        if row is None: return
-        
-        repo = data[row]
-        view["reponame"].text = repo["name"]
+        data = requests.get(url).json()  # normally returns a list of dicts
+    except requests.HTTPError as err:
+        return console.alert("User '{}' not found".format(username))
+    except Exception as err:
+        return console.alert("Error downloading metadata: {}".format(err))
+    if isinstance(data, dict) and data["message"] == "Not Found":
+        return console.alert("User '{}' not found".format(username))
+    repos = sorted([i["name"] for i in data])
+    rview = data_view("repo", repos)
+    rview.present("sheet")
+    rview.wait_modal()
+    tapped_text = rview["rtable"].delegate.selected_item
+    if tapped_text:
+        view["reponame"].text = tapped_text
 
-def release_view(releases):
-    rview = ui.View(name="Choose release")
-    lst   = ui.ListDataSource(releases)
-    delegate = Delegate()
-    
+def data_view(name, data):
+    rview = ui.View(name="Choose a " + name)
     table = ui.TableView()
     table.name = "rtable"
     table.flex = "WH"
-    table.data_source = lst
-    table.delegate = delegate
-    
-    rview.add_subview(table)
-    return rview
-
-def repo_view(repos):
-    rview = ui.View(name="Choose a repo")
-    lst   = ui.ListDataSource(repos)
-    delegate = Delegate()
-    
-    table = ui.TableView()
-    table.name = "rtable"
-    table.flex = "WH"
-    table.data_source = lst
-    table.delegate = delegate
-    
+    table.data_source = ui.ListDataSource(data)
+    table.data_source.delete_enabled = False
+    table.delegate = Delegate()
     rview.add_subview(table)
     return rview
 
 view = ui.load_view('gitrepo')
-cb = clipboard.get().strip()
-parse = urlparse.urlparse(cb)
+for name in 'username reponame'.split():
+    view[name].autocapitalization_type = ui.AUTOCAPITALIZE_NONE
+parse = urlparse.urlparse(clipboard.get().strip())
 if parse.scheme:
     path = [i for i in parse.path.split("/") if i]
     if len(path) >= 2:
-        view["username"].text = path[0]
-        view["reponame"].text = path[1]
+        view["username"].text, view["reponame"].text = path[:2]
 view.present('popover')
